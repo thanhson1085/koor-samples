44ce14ee4bfc61026ce3279693e94b19402f478a
root <thanhson1085@gmail.com>
2016-10-08 16:43:39 +0000

    first commit with mqtt sample :tulip:

---
+++ MqttClient/app/application.cpp
@@ -0,0 +1,74 @@
+#include <config.h>
+#include <app.h>
+#include <SmingCore/SmingCore.h>
+
+// Forward declarations
+void startMqttClient();
+void onMessageReceived(String topic, String message);
+
+Timer procTimer;
+int randValue = 50;
+
+// MQTT client
+MqttClient mqtt(MQTT_HOST, MQTT_PORT, onMessageReceived);
+
+// Publish our message
+void publishMessage()
+{
+	if (mqtt.getConnectionState() != eTCS_Connected)
+		startMqttClient(); // Auto reconnect
+        randValue = rand() % 100;
+	Serial.println("Let's publish message now!");
+	mqtt.publish(MQTT_TOPIC, String(randValue)); // or publishWithQoS
+}
+
+// Callback for messages, arrived from MQTT server
+void onMessageReceived(String topic, String message)
+{
+	Serial.print(topic);
+	Serial.print(":\r\n\t"); // Prettify alignment for printing
+	Serial.println(message);
+}
+
+// Run MQTT client
+void startMqttClient()
+{
+	if(!mqtt.setWill("last/will","The connection from this device is lost:(", 1, true)) {
+		debugf("Unable to set the last will and testament. Most probably there is not enough memory on the device.");
+	}
+	mqtt.connect("esp8266");
+	mqtt.subscribe(MQTT_TOPIC);
+}
+
+// Will be called when WiFi station was connected to AP
+void connectOk()
+{
+	Serial.println("I'm CONNECTED");
+
+	// Run MQTT client
+	startMqttClient();
+
+	// Start publishing loop
+	procTimer.initializeMs(20 * 1000, publishMessage).start(); // every 20 seconds
+}
+
+// Will be called when WiFi station timeout was reached
+void connectFail()
+{
+	Serial.println("I'm NOT CONNECTED. Need help :(");
+
+	// .. some you code for device configuration ..
+}
+
+void init()
+{
+	Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
+	Serial.systemDebugOutput(true); // Debug output to serial
+
+	WifiStation.config(WIFI_SSID, WIFI_PWD);
+	WifiStation.enable(true);
+	WifiAccessPoint.enable(false);
+
+	// Run our method when station was connected to AP (or not connected)
+	WifiStation.waitConnection(connectOk, 20, connectFail); // We recommend 20+ seconds for connection timeout at start
+}
